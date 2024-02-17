b0a02871175a536561e4351b0da1bfbce1d639c0
aritra <aritrarick2002@gmail.com>
2024-02-18 00:53:58 +0530

    Initial Commit

---
+++ UnCrack/UnCrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnCrackApp.swift
+//  UnCrack
+//
+//  Created by Aritra on 18/02/24.
+//
+
+import SwiftUI
+
+@main
+struct UnCrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
